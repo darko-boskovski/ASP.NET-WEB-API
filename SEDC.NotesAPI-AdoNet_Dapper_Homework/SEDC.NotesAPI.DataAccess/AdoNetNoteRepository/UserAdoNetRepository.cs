af70fad5c615c053d30bd3c630c48c14485e7974
darko-boskovski <darko_b23@hotmail.com>
2021-10-03 11:13:31 +0200

    WebApi_AdoNet/Dapper Homework init

---
+++ SEDC.NotesAPI-AdoNet_Dapper_Homework/SEDC.NotesAPI.DataAccess/AdoNetNoteRepository/UserAdoNetRepository.cs
@@ -0,0 +1,141 @@
+﻿using SEDC.NotesAPI.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace SEDC.NotesAPI.DataAccess.AdoNetNoteRepository
+{
+    public class UserAdoNetRepository : IRepository<User>
+    {
+
+           private string _connectionString;
+            public UserAdoNetRepository(string connectionString)
+            {
+                _connectionString = connectionString;
+            }
+
+            public void Add(User entity)
+            {
+                SqlConnection sqlConnection = new SqlConnection(_connectionString);
+                sqlConnection.Open();
+
+                SqlCommand command = new SqlCommand();
+                command.Connection = sqlConnection;
+
+                command.CommandText = $@"INSERT INTO Users(FirstName, LastName, UserName)
+                VALUES(@UserFirstName, @UserLastName, @UserUserName)";
+                command.Parameters.AddWithValue("@UserFirstName", entity.FirstName);
+                command.Parameters.AddWithValue("@UserLastName", entity.LastName);
+                command.Parameters.AddWithValue("@UserUserName", entity.Username);
+
+
+
+            command.ExecuteNonQuery(); 
+                sqlConnection.Close();
+            }
+
+            public void Delete(int id)
+            {
+                SqlConnection sqlConnection = new SqlConnection(_connectionString);
+                sqlConnection.Open();
+
+                SqlCommand command = new SqlCommand();
+                command.Connection = sqlConnection;
+
+                command.CommandText = "DELETE FROM Users WHERE Id = @id";
+                command.Parameters.AddWithValue("@id", id);
+                command.ExecuteNonQuery();
+                sqlConnection.Close();
+            }
+
+            public List<User> GetAll()
+            {
+               
+                SqlConnection sqlConnection = new SqlConnection(_connectionString);
+
+            
+                sqlConnection.Open();
+
+                
+                SqlCommand command = new SqlCommand();
+
+               
+                command.Connection = sqlConnection;
+
+              
+                command.CommandText = "SELECT * FROM dbo.Users";
+
+                List<User> usersDb = new List<User>();
+
+               
+                SqlDataReader reader = command.ExecuteReader();
+
+              
+                while (reader.Read())
+                {
+                    usersDb.Add(new User
+                    {
+                        Id = (int)reader["Id"],
+                        FirstName = (string)reader["FirstName"],
+                        LastName = (string)reader["LastName"],
+                        Username = (string)reader["UserName"],
+                    });
+                }
+
+                sqlConnection.Close();
+
+                return usersDb;
+            }
+
+            public User GetById(int id)
+            {
+                SqlConnection sqlConnection = new SqlConnection(_connectionString);
+                sqlConnection.Open();
+
+                SqlCommand command = new SqlCommand();
+                command.Connection = sqlConnection;
+
+                command.CommandText = $"SELECT * FROM dbo.Users WHERE Id = @id";
+                command.Parameters.AddWithValue("@id", id);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                List<User> usersDb = new List<User>();
+
+                while (reader.Read())
+                {
+                    usersDb.Add(new User
+                    {
+                        Id = (int)reader["Id"],
+                        FirstName = (string)reader["FirstName"],
+                        LastName = (string)reader["LastName"],
+                        Username = (string)reader["UserName"],
+                    });
+                }
+
+                sqlConnection.Close();
+
+                return usersDb.FirstOrDefault();
+            }
+
+            public void Update(User entity)
+            {
+                SqlConnection sqlConnection = new SqlConnection(_connectionString);
+                sqlConnection.Open();
+
+                SqlCommand command = new SqlCommand();
+                command.Connection = sqlConnection;
+
+                command.CommandText = $@"UPDATE dbo.Users SET Id = @UserId, FirstName  @UserFirstName, LastName = @UserLastName,UserName = @UserUserName 
+                WHERE Id = @Id";
+                command.Parameters.AddWithValue("@UserId", entity.Id);
+                command.Parameters.AddWithValue("@UserFirstName", entity.FirstName);
+                command.Parameters.AddWithValue("@UserLastName", entity.LastName);
+
+                command.ExecuteNonQuery();
+                sqlConnection.Close();
+            }
+        }
+}
---
+++ SEDC.NotesAPI-AdoNet_Dapper_Homework/SEDC.NotesAPI/Controllers/UsersController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SEDC.NotesAPI.Domain.Models;
+using SEDC.NotesAPI.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.NotesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<User>> Get()
+        {
+            return StatusCode(StatusCodes.Status200OK, _userService.GetAllUsers());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<User> Get(int id)
+        {
+            return StatusCode(StatusCodes.Status200OK, _userService.GetUserById(id));
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] User user)
+        {
+            _userService.AddUser(user);
+            return StatusCode(StatusCodes.Status201Created, "User created!");
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] User user)
+        {
+            _userService.UpdateUser(user);
+            return StatusCode(StatusCodes.Status204NoContent, "User updated!");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _userService.DeleteUser(id);
+            return StatusCode(StatusCodes.Status204NoContent, "User deleted!");
+        }
+    }
+}
